673a8144bc5ab577be6ae4197c5ed3cc54aa75ff
eeue56 <enalicho@gmail.com>
2022-12-11 20:07:30 +0100

    introduce Html as an AST for rendering
    To render content into the DOM, it's handy to have an Abstract Syntax Tree (AST) which represents the content to be rendered. In Elm, this is provided via the Html package. In React, JSX provides a user-familiar syntax while still turning into Reacty type stuff. In this example, we create our AST with 3 constructor functions that users can call - instead of using another templating language, make-your-own-tea focuses on everything being a function. Once we have our AST, we can use that the render and fill the DOM with actual elements.

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,46 @@
+// To provide a way to do patching and generation of html,
+// we build up an abstract syntax tree (AST).
+// Working with an AST allows you to provide a higher level API
+// for users of your library, while also restricting what's possible.
+// The first half of the AST are Nodes - these map directly to HTML tags.
+// For now, we'll allow them all to have children. Think of a div inside a div.
+type Node = {
+    kind: "Node";
+    tag: "div" | "h1";
+    children: Html[];
+};
+
+// The second half of the AST are TextNodes - the string content inside a HTML tag
+// For example <div>Hello world</div> would be Node("div", [ TextNode("Hello world") ])
+type TextNode = { kind: "Text"; content: string };
+
+type Html = Node | TextNode;
+
+// To provide users of the library with better auto complete and restrict the inside baseball
+// of how the AST looks, we provide these helper functions.
+function div(children: Html[]): Html {
+    return {
+        kind: "Node",
+        tag: "div",
+        children,
+    };
+}
+
+function h1(children: Html[]): Html {
+    return {
+        kind: "Node",
+        tag: "h1",
+        children,
+    };
+}
+
+function text(content: string): Html {
+    return {
+        kind: "Text",
+        content,
+    };
+}
+
 // The basics of the Elm architecture.
 // Every running program is based on a model, which is the data store
 // and the message, which is how interactions or events are sent to the program.
@@ -8,12 +51,29 @@
 type Program<model, message> = {
     initialModel: model;
     update(message: message, model: model): model;
-    view(model: model): string;
+    view(model: model): Html;
 };
 
 // Once we've started a program, we'll want a type to represent it.
 // For now, we'll just define an empty object.
 type RunningProgram = {};
+
+// We need some way of turning our AST into actual things the DOM API can use
+// so Nodes are turned into HTMLElements, and TextNodes are turned into Text.
+function buildTree(html: Html): HTMLElement | Text {
+    switch (html.kind) {
+        case "Node": {
+            const node = document.createElement(html.tag);
+            for (const child of html.children) {
+                node.appendChild(buildTree(child));
+            }
+            return node;
+        }
+        case "Text": {
+            return document.createTextNode(html.content);
+        }
+    }
+}
 
 // Takes a program, then actually calls the related functions.
 // Populates a root tag with the content provided by the view function.
@@ -27,7 +87,8 @@ function runProgram<model, message>(
 
     const root = document.getElementById("root");
     if (root) {
-        root.innerHTML = view;
+        // we now replace the children of the root element with the elements
+        root.replaceChildren(buildTree(view));
     } else {
         console.error(
             "You forgot to define a <div id='root'></div> inside body"
@@ -64,8 +125,11 @@ function update(message: Message, model: Model): Model {
 }
 
 // Our view function.
-function view(model: Model): string {
-    return `Hi ${model.name}`;
+function view(model: Model): Html {
+    return div([
+        h1([ text("Hi there") ]),
+        div([ text(`Welcome ${model.name}`) ]),
+    ]);
 }
 
 // Actually run the program.
